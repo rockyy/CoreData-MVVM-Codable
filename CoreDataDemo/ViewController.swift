42bb0b9a137c0bae1a1f6efef358e33d37e45e4a
RakeshKumarNayak <rakesh.k@girnarsoft.com>
2019-09-01 12:43:51 +0530

    Initial project added

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Rakesh Kumar on 01/09/19.
+//  Copyright © 2019 Rakesh Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
